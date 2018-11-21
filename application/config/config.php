edc3b4c86155b8feea7245b6fe075a0327a999be
Gabriel Capelli <gcapelli@lynxview.es>
2018-11-21 19:14:23 +0100

    a ver

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'anda que';
+$config['base_url'] = 'anda que no lo se';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhosttiti',
 	'username' => '',
 	'password' => '',
 	'database' => '',
